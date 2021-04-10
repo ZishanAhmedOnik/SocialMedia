4060dd5036ed987d939adb4fd3eb88799ed03efe
Zishan Onik <zonik42@tntech.edu>
2021-04-10 13:02:45 -0500

    End of section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -22,9 +22,7 @@ export class AccountService {
           const user = response;
 
           if (user) {
-            localStorage.setItem('user', JSON.stringify(user));
-
-            this.currentUserSource.next(user);
+            this.setCurrentUser(user);
           }
 
           return user;
@@ -37,9 +35,7 @@ export class AccountService {
       .pipe(
         map((user: User) => {
           if(user) {
-            localStorage.setItem('user', JSON.stringify(user));
-
-            this.currentUserSource.next(user);
+            this.setCurrentUser(user);
           }
 
           return user;
@@ -48,6 +44,8 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+
     this.currentUserSource.next(user);
   }
 
